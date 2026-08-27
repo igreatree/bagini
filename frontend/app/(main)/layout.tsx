26759af906c01d7495c59d1bcafb27008c4cef45
ig <gaibullaeviz@gmail.com>
2026-08-27 14:06:04 +0300

    feature: add mantine

---
+++ frontend/app/(main)/layout.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { AppShell, Burger, Group, UnstyledButton } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    const [opened, { toggle }] = useDisclosure();
+
+    return (
+        <AppShell
+            header={{ height: 60 }}
+            navbar={{
+                width: 300,
+                breakpoint: "sm",
+                collapsed: { desktop: true, mobile: !opened },
+            }}
+            padding="md"
+        >
+            <AppShell.Header>
+                <Group h="100%" px="md">
+                    <Burger
+                        opened={opened}
+                        onClick={toggle}
+                        hiddenFrom="sm"
+                        size="sm"
+                    />
+                    <Group justify="space-between" style={{ flex: 1 }}>
+                        <Image
+                            src="/logo.svg"
+                            alt="Bagini logo"
+                            width={90}
+                            height={37}
+                            loading="eager"
+                        />
+                        <Group ml="xl" gap={0} visibleFrom="sm">
+                            <UnstyledButton component={Link} href="/">
+                                Главная
+                            </UnstyledButton>
+                            <UnstyledButton component={Link} href="about">
+                                О нас
+                            </UnstyledButton>
+                            <UnstyledButton>Контакты</UnstyledButton>
+                        </Group>
+                    </Group>
+                </Group>
+            </AppShell.Header>
+
+            <AppShell.Navbar py="md" px={4}>
+                <UnstyledButton>Главная</UnstyledButton>
+                <UnstyledButton>О нас</UnstyledButton>
+                <UnstyledButton>Контакты</UnstyledButton>
+            </AppShell.Navbar>
+
+            <AppShell.Main>{children}</AppShell.Main>
+        </AppShell>
+    );
+}
---
+++ frontend/app/(main)/page.tsx
@@ -0,0 +1,5 @@
+import { Title } from "@mantine/core";
+
+export default function Main() {
+    return <Title>Main</Title>;
+}
