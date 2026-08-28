d2a9b56c8332c3fce245621a26047bf817770c73
ig <gaibullaeviz@gmail.com>
2026-08-28 21:35:21 +0300

    bugfix

--- backend/src/redis/redis.module.ts
+++ backend/src/redis/redis.module.ts
@@ -7,9 +7,8 @@ const redisProvider: Provider = {
     provide: REDIS_CLIENT,
     useFactory: () => {
         return new Redis({
-            host: "localhost",
-            port: 6379,
-            password: process.env.REDIS_PASSWORD || undefined,
+            host: process.env.REDIS_HOST || "localhost",
+            port: parseInt(process.env.REDIS_PORT || "6379", 10),
         });
     },
 };
